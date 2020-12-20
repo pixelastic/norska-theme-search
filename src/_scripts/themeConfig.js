347319100749a3984bf27076c88819090cea1a50
Tim Carry <tim@pixelastic.com>
2020-12-20 21:55:23 +0100

    Initial commit for dummy release

---
+++ src/_scripts/themeConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  options: {
+    placeholder: 'Search for anything', // Value of the search placeholder
+    hitName: 'item', // Name of the items to search
+    transforms: {}, // Transforms to apply to the hits
+    widgets: [], // Additional widgets to add to instantsearch
+  },
+};
---
+++ src/script.js
@@ -0,0 +1,10 @@
+// const infiniteMasonry = require('./_scripts/infiniteMasonry');
+// const themeConfig = require('./_scripts/themeConfig.js');
+
+module.exports = {
+  async init() {
+    // themeConfig.options = { ...themeConfig.options, ...options };
+    // infiniteMasonry.initSearch();
+    // infiniteMasonry.initLazyload();
+  },
+};
