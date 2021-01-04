e49d165b976cd3ebef8d91b3b16f1e1659af1454
Tim Carry <tim@pixelastic.com>
2021-01-04 21:06:59 +0100

    Add header

--- src/script.js
+++ src/script.js
@@ -1,10 +1,80 @@
-// const infiniteMasonry = require('./_scripts/infiniteMasonry');
-// const themeConfig = require('./_scripts/themeConfig.js');
+const lazyload = require('norska/frontend/lazyload');
+const algolia = require('norska/frontend/algolia');
+const {
+  configure,
+  hits,
+  refinementList,
+  searchBox,
+} = require('norska/frontend/algolia/widgets');
+const algoliaCredentials = window.CONFIG.algolia;
+
+const widgets = [
+  /**
+   * Main configuration
+   **/
+  {
+    type: configure,
+    options: {
+      hitsPerPage: 24,
+    },
+  },
+  /**
+   * Searchbar
+   **/
+  {
+    type: searchBox,
+    options: {
+      container: '#searchbox',
+      placeholder: 'Type any keyword...',
+      autofocus: true,
+      showReset: false,
+      showSubmit: false,
+      showLoadingIndicator: false,
+    },
+  },
+  /**
+   * Hits
+   **/
+  {
+    type: hits,
+    options: {
+      container: '#hits',
+      templates: {
+        item: document.getElementById('hitTemplate').value,
+        empty: document.getElementById('emptyTemplate').value,
+      },
+    },
+  },
+  // {
+  //   type: pagination,
+  //   options: {
+  //     container: '#pagination',
+  //   },
+  // },
+  /**
+   * Sidebar filtering
+   **/
+  {
+    type: refinementList,
+    options: {
+      container: '#filterBook',
+      attribute: 'book',
+      sortBy: ['name:asc'],
+    },
+  },
+];
 
 module.exports = {
   async init() {
-    // themeConfig.options = { ...themeConfig.options, ...options };
-    // infiniteMasonry.initSearch();
-    // infiniteMasonry.initLazyload();
+    algolia
+      .init(algoliaCredentials)
+      .setWidgets(widgets)
+      // .setTransforms(transforms)
+      // .onDisplay(hit => {
+      //   console.info(hit.picture);
+      // })
+      .start();
+
+    lazyload.init();
   },
 };
