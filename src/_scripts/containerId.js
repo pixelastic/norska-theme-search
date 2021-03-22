c3d5afed63c76ddf3a686de6e433606ccc55a229
Tim Carry <tim@pixelastic.com>
2021-03-22 14:46:55 +0100

    Fix containerId for nested keys

--- src/_scripts/containerId.js
+++ src/_scripts/containerId.js
@@ -1,6 +1,6 @@
 // Generate a unique name from a widgetDefinition
 module.exports = (widget) => {
   const attribute = widget && widget.options && widget.options.attribute;
-  const slug = attribute.replace('.', '-');
+  const slug = attribute.replace(/\./g, '-');
   return `filter-${slug}`;
 };
