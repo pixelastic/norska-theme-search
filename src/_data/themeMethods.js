ee6b1385a938d7e23305aa8242eb6b1df70c571f
Tim Carry <tim@pixelastic.com>
2021-07-17 21:51:17 +0200

    Make templates easier to overwrite

--- src/_data/themeMethods.js
+++ src/_data/themeMethods.js
@@ -1,15 +1,6 @@
 const containerId = require('../_scripts/containerId');
 const shouldHideWidget = require('../_scripts/shouldHideWidget');
 module.exports = {
-  // Load the file in the theme, or the one in the host
-  template(templateName, { include }) {
-    const templatePath = `_includes/templates/${templateName}.pug`;
-    try {
-      return include(templatePath);
-    } catch (_err) {
-      return `Overwrite this template by creating ${templatePath}`;
-    }
-  },
   // Unique identifier for a container
   containerId,
   // Check if a specific widget should be hidden
